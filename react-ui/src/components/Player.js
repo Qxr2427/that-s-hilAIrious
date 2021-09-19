ed5bfd281965a632c177fcaf07033d96da2a475a
Alex Chan <alex.chan@shopify.com>
2021-09-19 11:57:40 -0400

    final

--- react-ui/src/components/Player.js
+++ react-ui/src/components/Player.js
@@ -145,10 +145,10 @@ const Player = ({ isLocalParticipant, player, socket, roomSid, inGame, curID }) 
     
     <div className="player">
       
-      {(funny_status === 'unfunny') && <h2 id="overlay">UNFUNNY! :(</h2>}
-      {(funny_status === 'funny') && <h2 id="overlay">FUNNY!</h2>}
-      {(funny_status === 'really-funny') && <h2 id="overlay">really FUNNY!</h2>}
-      {(funny_status === 'hilarious') && <h2 id="overlay">hILaRiOuS!</h2>}
+      {(funny_status === 'unfunny') && <h2 id="overlay">👎</h2>}
+      {(funny_status === 'funny') && <h2 id="overlay">🔥</h2>}
+      {(funny_status === 'really-funny') && <h2 id="overlay">😆</h2>}
+      {(funny_status === 'hilarious') && <h2 id="overlay">😂</h2>}
       
       
       <video id="player-video" ref={videoRef} autoPlay={true} style={{ transform: isLocalParticipant ? 'rotateY(180deg)' : '' }}/>
